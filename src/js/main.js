29991b362c839763a690bf2710e3c1083d65e3ed
daramzy <daramzycoder@gmail.com>
2021-12-02 14:53:42 +0900

    feat : 복사 방지 추가

--- src/js/main.js
+++ src/js/main.js
@@ -332,8 +332,7 @@ document.addEventListener('dblclick', (e) => {
 //     return false;
 // })
 
-window.oncontextmenu = function(event) {
+document.oncontextmenu = function(event) {
     event.preventDefault(); // 기본 태그 기능 막기
-    event.stopPropagation(); // 이벤트 전달 막기
     return false;
-  };
+};
